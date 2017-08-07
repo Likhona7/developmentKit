f17ca15fd4ad74a5a88f81713926acb7daa24d73
likhona7 <likhona@projectcodex.co>
2017-08-07 13:21:58 +0200

    added packages

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,2 @@
+import chalk from 'chalk';
+console.log(chalk.blue('*****') + " " + chalk.yellow('Starting app in dev mode... on localhost:3000') +" "+ chalk.blue('*****')); //eslint-disable-line no-console
