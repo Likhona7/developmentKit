d596dccbec6e1af548b7cf4ae12322a3208114c6
likhona7 <likhona@projectcodex.co>
2017-08-13 12:45:24 +0200

    in process of build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,20 +1,53 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'bundle.js'
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+      filename: '[name].[chunkhash].js'
   },
   plugins: [
+    //generating an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    // Hash the files using MD5 so that their names change when the content changes.
+    new webpackMd5Hash(),
+    //Use CommonsChunkPlugin to create a separate bundle
+    // of vendor libaries so that they're cached separately.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    //Createthat include reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true,
+      // Properties you define here are available in index.html
+      // using HtmlWebpackPlugin.options.varName
+      trackJSToken: '146ac8848609498692b02b6dadeaa488'
+    }),
+
     // Eliminate duplicate packages when generating bundle
     new webpack.optimize.DedupePlugin(),
 
@@ -22,15 +55,9 @@ export default {
     new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
-    loaders: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ['babel']
-      },
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css']
-      }
+    loaders:
+      [{test: /\.js$/,exclude: /node_modules/,loaders: ['babel']},
+      {test: /\.css$/,loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 }
